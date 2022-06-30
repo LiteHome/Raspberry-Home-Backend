991c29f7a5cbacec75d8c14010550d88e1b78635
houance <837507557@qq.com>
2022-06-30 17:45:36 +0800

    1. set up project

---
+++ rashome/src/main/java/com/rashome/rashome/RashomeApplication.java
@@ -0,0 +1,13 @@
+package com.rashome.rashome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RashomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RashomeApplication.class, args);
+	}
+
+}
---
+++ rashome/src/test/java/com/rashome/rashome/RashomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rashome.rashome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RashomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

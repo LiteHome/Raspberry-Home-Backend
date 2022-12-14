86f51e221febe6834c1c4cca204b620e22ed914e
houance <837507557@qq.com>
2022-12-15 00:43:27 +0800

    1. 完善 basecontroller, 引入 logger

--- rashome/src/main/java/com/iot/rashome/controller/SensorEnviromentController.java
+++ rashome/src/main/java/com/iot/rashome/controller/SensorEnviromentController.java
@@ -1,14 +1,9 @@
 package com.iot.rashome.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.iot.rashome.base.BaseController;
-import com.iot.rashome.dto.Request;
 import com.iot.rashome.service.SensorEnviromentService;
 import com.iot.rashome.vo.SensorEnviromentVO;
 
@@ -16,8 +11,13 @@ import com.iot.rashome.vo.SensorEnviromentVO;
 @RequestMapping("/sensorEnviroment")
 public class SensorEnviromentController extends BaseController<SensorEnviromentService, SensorEnviromentVO> {
 
-    @PostMapping("/")
-    public void insert(@RequestBody Request request) throws JsonMappingException, JsonProcessingException{
-        realInsert(request, SensorEnviromentVO.class);
+    @Override
+    public Class<SensorEnviromentVO> setVoType() {
+        return SensorEnviromentVO.class;
+    }
+
+    @Override
+    public Class<?> setChildClass() {
+        return SensorEnviromentController.class;
     }
 }
